15683e25e33f39110a0d419a0c4e654eb26c43c4
Andrew Watson <awatson@thoughtworks.com>
2016-12-07 12:18:22 +1100

    A page with stuff in it

---
+++ app/Page/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Page extends Component {
+  render() {
+    return (
+      <h1>Hello react!</h1>
+    );
+  }
+}
+
+export default Page;
--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-console.log('Hello webpack!');
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+import Page from 'Page';
+
+var page = <Page />;
+
+ReactDOM.render(
+  page,
+  document.getElementById('target')
+);
